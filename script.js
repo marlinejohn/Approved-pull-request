34624a60032165bb8d9338dc626890e04c01eb09
Marline John <marlinejohn@gmail.com>
2024-01-13 00:47:59 +0100

    Created script file and initialized Reviews object/dictionary

---
+++ script.js
@@ -0,0 +1,14 @@
+
+  const latestCommitTimestamp = Date.now();
+  const reviews = [
+    { userId: "user1", timestamp: Date.now() - 1000, reviewType: "APPROVAL" },
+    { userId: "user2", timestamp: Date.now() + 10, reviewType: "COMMENT" },
+    { userId: "user1", timestamp: Date.now() + 10, reviewType: "APPROVAL" },
+    { userId: "user2", timestamp: Date.now() + 20, reviewType: "COMMENT" },
+    { userId: "user1", timestamp: Date.now() + 30, reviewType: "APPROVAL" },
+    { userId: "user3", timestamp: Date.now() + 35, reviewType: "APPROVAL" },
+  ];
+  
+checkPullRequestApproval(latestCommitTimestamp, reviews);
+  
+  
